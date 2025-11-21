1d351602c747701b28ff8046b111283b353aa734
le minh ngoc <ngoc.le1@ucalgary.ca>
2025-11-20 22:45:53 -0700

    remove redundant test cases and make the code looks shorter

--- src/graph.py
+++ src/graph.py
@@ -1,23 +1,12 @@
 import networkx as nx
-import cnf_parser
 from itertools import combinations
-SAT, num_of_var, num_of_clauses = cnf_parser.parse_dimacs("test.cnf")
-G = nx.Graph()
-combination = combinations
-edges = []
-G.add_nodes_from(range(1, num_of_var + 1))
-for clause in SAT:
-    for i in clause:
-        if i > 0:
-            edges.append(i)
-        elif i < 0:
-            edges.append(abs(i))
-    connection = (combination(edges,2))
-    edges.clear()
-    for i in connection:
-        G.add_edge(*i)
-
-"""
-[1, 2, -3]	(1, 2), (1, 3), (2, 3)	
-[-3, 4]	(3, 4)	
-"""
+def build_graph(num_of_var, clauses):
+    G = nx.Graph()
+    # 1. Add all nodes 
+    G.add_nodes_from(range(1, num_of_var + 1))
+    # 2. Add edges (Clique logic)
+    for clause in clauses:
+        clause_vars = [abs(x) for x in clause]
+        for i in combinations(clause_vars, 2):
+            G.add_edge(*i)
+    return G
